3f1c2625c6afb718a6b44c8ef6d9baa66e008148
ssssota <tommysouta@gmail.com>
2023-01-29 02:08:41 +0900

    fix: move type definitons to avoid a type error

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,45 @@
 import type { MethodKind } from "@bufbuild/protobuf";
 import { ServiceType } from "@bufbuild/protobuf";
-import { ResponseComposition, rest, RestContext, RestRequest } from "msw";
+import {
+	ResponseComposition,
+	ResponseTransformer,
+	rest,
+	RestContext,
+	RestHandler,
+	RestRequest,
+} from "msw";
 
 type PromiseOr<T> = T | Promise<T>;
 
-export const createMswConnectWeb = (baseUrl: string) => {
-	return <S extends ServiceType, M extends Exclude<keyof S["methods"], symbol>>(
-		service: S,
-		method: M,
-		handler: S["methods"][M]["kind"] extends MethodKind.Unary
-			? (
-					req: RestRequest & { message(): InstanceType<S["methods"][M]["I"]> },
-					res: ResponseComposition,
-					ctx: RestContext & {
-						message(
-							msg: InstanceType<S["methods"][M]["O"]>,
-						): RestContext["json"];
-					},
-			  ) => PromiseOr<ReturnType<ResponseComposition>>
-			: never,
-	) => {
+type CreateMswConnectWeb = (baseUrl: string) => <
+	S extends ServiceType,
+	M extends Exclude<keyof S["methods"], symbol>,
+>(
+	service: S,
+	method: M,
+	handler: S["methods"][M]["kind"] extends MethodKind.Unary
+		? (
+				req: RestRequest & {
+					message(): Promise<InstanceType<S["methods"][M]["I"]>>;
+				},
+				res: ResponseComposition,
+				ctx: RestContext & {
+					message(msg: InstanceType<S["methods"][M]["O"]>): ResponseTransformer;
+				},
+		  ) => PromiseOr<ReturnType<ResponseComposition>>
+		: never,
+) => RestHandler;
+
+export const createMswConnectWeb: CreateMswConnectWeb = (baseUrl) => {
+	return (service, method, handler) => {
 		const methodInfo = service.methods[method];
 		return rest.post(
 			// https://github.com/bufbuild/connect-web/blob/a85b04b834112512df245a98768560e304717889/packages/connect-web/src/connect-transport.ts#L123
 			baseUrl.replace(/\/?$/, `/${service.typeName}/${methodInfo.name}`),
 			async (req, res, ctx) => {
 				type HandlerArgs = Parameters<typeof handler>;
+				type ExReq = HandlerArgs[0];
+				type ExCtx = HandlerArgs[2];
 				const exReq = new Proxy(req, {
 					get(target, p, receiver) {
 						if (p === "message") {
@@ -33,17 +47,17 @@ export const createMswConnectWeb = (baseUrl: string) => {
 						}
 						return Reflect.get(target, p, receiver);
 					},
-				}) as HandlerArgs[0];
+				}) as ExReq;
 				const exCtx = new Proxy(ctx, {
 					get(target, p, receiver) {
 						if (p === "message") {
-							return (msg: InstanceType<S["methods"][M]["O"]>) => {
+							return ((msg) => {
 								return ctx.json(msg.toJson());
-							};
+							}) satisfies ExCtx["message"];
 						}
 						return Reflect.get(target, p, receiver);
 					},
-				}) as HandlerArgs[2];
+				}) as ExCtx;
 				return handler(exReq, res, exCtx);
 			},
 		);
