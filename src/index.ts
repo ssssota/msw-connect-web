51f9104d33ae95b812cde7e38d5902caa612d26f
ssssota <tommysouta@gmail.com>
2023-01-29 01:33:06 +0900

    fix!: api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { MethodKind } from "@bufbuild/protobuf";
 import { ServiceType } from "@bufbuild/protobuf";
-import { rest } from "msw";
+import { ResponseComposition, rest, RestContext, RestRequest } from "msw";
 
 type PromiseOr<T> = T | Promise<T>;
 
@@ -10,20 +10,41 @@ export const createMswConnectWeb = (baseUrl: string) => {
 		method: M,
 		handler: S["methods"][M]["kind"] extends MethodKind.Unary
 			? (
-					req: InstanceType<S["methods"][M]["I"]>,
-			  ) => PromiseOr<InstanceType<S["methods"][M]["O"]>>
+					req: RestRequest & { message(): InstanceType<S["methods"][M]["I"]> },
+					res: ResponseComposition,
+					ctx: RestContext & {
+						message(
+							msg: InstanceType<S["methods"][M]["O"]>,
+						): RestContext["json"];
+					},
+			  ) => PromiseOr<ReturnType<ResponseComposition>>
 			: never,
 	) => {
-		type InputMessage = InstanceType<S["methods"][M]["I"]>;
 		const methodInfo = service.methods[method];
 		return rest.post(
 			// https://github.com/bufbuild/connect-web/blob/a85b04b834112512df245a98768560e304717889/packages/connect-web/src/connect-transport.ts#L123
 			baseUrl.replace(/\/?$/, `/${service.typeName}/${methodInfo.name}`),
 			async (req, res, ctx) => {
-				const reqJson = await req.json();
-				const reqMessage = methodInfo.I.fromJson(reqJson);
-				const resMessage = await handler(reqMessage as InputMessage);
-				return res(ctx.json(resMessage.toJson()));
+				type HandlerArgs = Parameters<typeof handler>;
+				const exReq = new Proxy(req, {
+					get(target, p, receiver) {
+						if (p === "message") {
+							return () => target.json().then((j) => methodInfo.I.fromJson(j));
+						}
+						return Reflect.get(target, p, receiver);
+					},
+				}) as HandlerArgs[0];
+				const exCtx = new Proxy(ctx, {
+					get(target, p, receiver) {
+						if (p === "message") {
+							return (msg: InstanceType<S["methods"][M]["O"]>) => {
+								return ctx.json(msg.toJson());
+							};
+						}
+						return Reflect.get(target, p, receiver);
+					},
+				}) as HandlerArgs[2];
+				return handler(exReq, res, exCtx);
 			},
 		);
 	};
